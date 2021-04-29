f280cb4daccfef482abafc9f445305b5823b89e1
tnobile <takao.nosaka@outlook.com>
2021-04-29 23:37:09 +0200

    use format

--- pages/components/date.js
+++ pages/components/date.js
@@ -1,9 +1,8 @@
 import { parseISO, format } from 'date-fns'
 
 export default function Date({ dateString }) {
-    if(!dateString)return "20220202";
+    if (!dateString) return "20220202";
     console.log("parsing date:", dateString);
-  const date = parseISO(dateString)
-  //return <time dateTime={dateString}>{format(date, 'LLLL d, yyyy')}</time>
-  return dateString;
+    const date = parseISO(dateString)
+    return <time dateTime={dateString}>{format(date, 'LLLL d, yyyy')}</time>
 }
