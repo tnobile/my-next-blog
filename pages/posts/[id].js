bc066b8c3b0915ecbb20236b73501aebe3940b19
tnobile <takao.nosaka@outlook.com>
2021-04-29 23:30:08 +0200

    fix with hard coded dates

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -16,6 +16,7 @@ export async function getStaticProps({ params }) {
 
 export async function getStaticPaths() {
     const paths = getAllPostIds()
+    console.log("static paths:", paths);
     return {
         paths,
         fallback: false
