fa7650f7309c492ab5f44967e5f2526c352544e9
tnobile <takao.nosaka@outlook.com>
2021-04-30 20:43:44 +0200

    static export

--- pages/components/layout.js
+++ pages/components/layout.js
@@ -1,5 +1,4 @@
 import Head from 'next/head'
-import Image from 'next/image'
 import styles from './layout.module.css'
 import utilStyles from '../../styles/utils.module.css'
 import Link from 'next/link'
@@ -28,12 +27,12 @@ export default function Layout({ children, home }) {
       <header className={styles.header}>
         {home ? (
           <>
-            <Image
+            <image
               priority
               src="/images/profile.jpg"
               className={utilStyles.borderCircle}
-              height={144}
-              width={144}
+              height={108}
+              width={108}
               alt={name}
             />
             <h1 className={utilStyles.heading2Xl}>{name}</h1>
@@ -42,7 +41,7 @@ export default function Layout({ children, home }) {
           <>
             <Link href="/">
               <a>
-                <Image
+                <image
                   priority
                   src="/images/profile.jpg"
                   className={utilStyles.borderCircle}
